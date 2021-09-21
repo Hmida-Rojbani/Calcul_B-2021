aac1c714076bede1fbfaae15b5973f0916135116
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-09-21 10:33:12 +0100

    aum

---
+++ src/main/java/de/tekup/calcul/CalculBApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.calcul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculBApplication.class, args);
+	}
+
+}
